28a1814a5976c0e461d1608ea7ef105153d58521
Michał Stanecki <stanustanley@gmail.com>
2017-04-02 14:06:59 +0200

    v4

---
+++ TAW-Magazyn-BranchStanaaa/WebServer/WebServer/Classes/LoginPassword.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebServer.Classes
+{
+    public class LoginPassword
+    {
+        public string login;
+        public string password;
+    }
+}
