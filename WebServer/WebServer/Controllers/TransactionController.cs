bcad26f3e55b74e990028686a8e0c2ca70710a28
SideswipeN7 <pawel.wanot@gmail.com>
2017-04-03 21:20:46 +0200

    #30 Created TransactionController

---
+++ WebServer/WebServer/Controllers/TransactionController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebServer.Models;
+
+namespace WebServer.Controllers
+{
+    public class TransactionController : ApiController
+    {
+    //    private magazynEntities db = new magazynEntities();
+
+    //    // GET: api/Transaction
+    //    public IQueryable<Transakcja> GetTransakcje()
+    //    {
+    //        return db.Transakcje;
+    //    }
+
+    //    // GET: api/Transaction/5
+    //    [ResponseType(typeof(Transakcja))]
+    //    public IHttpActionResult GetTransakcja(int id)
+    //    {
+    //        Transakcja transakcja = db.Transakcje.Find(id);
+    //        if (transakcja == null)
+    //        {
+    //            return NotFound();
+    //        }
+
+    //        return Ok(transakcja);
+    //    }
+
+    //    // PUT: api/Transaction/5
+    //    [ResponseType(typeof(void))]
+    //    public IHttpActionResult PutTransakcja(int id, Transakcja transakcja)
+    //    {
+    //        if (!ModelState.IsValid)
+    //        {
+    //            return BadRequest(ModelState);
+    //        }
+
+    //        if (id != transakcja.idTransakcji)
+    //        {
+    //            return BadRequest();
+    //        }
+
+    //        db.Entry(transakcja).State = EntityState.Modified;
+
+    //        try
+    //        {
+    //            db.SaveChanges();
+    //        }
+    //        catch (DbUpdateConcurrencyException)
+    //        {
+    //            if (!TransakcjaExists(id))
+    //            {
+    //                return NotFound();
+    //            }
+    //            else
+    //            {
+    //                throw;
+    //            }
+    //        }
+
+    //        return StatusCode(HttpStatusCode.NoContent);
+    //    }
+
+    //    // POST: api/Transaction
+    //    [ResponseType(typeof(Transakcja))]
+    //    public IHttpActionResult PostTransakcja(Transakcja transakcja)
+    //    {
+    //        if (!ModelState.IsValid)
+    //        {
+    //            return BadRequest(ModelState);
+    //        }
+
+    //        db.Transakcje.Add(transakcja);
+    //        db.SaveChanges();
+
+    //        return CreatedAtRoute("DefaultApi", new { id = transakcja.idTransakcji }, transakcja);
+    //    }
+
+    //    // DELETE: api/Transaction/5
+    //    [ResponseType(typeof(Transakcja))]
+    //    public IHttpActionResult DeleteTransakcja(int id)
+    //    {
+    //        Transakcja transakcja = db.Transakcje.Find(id);
+    //        if (transakcja == null)
+    //        {
+    //            return NotFound();
+    //        }
+
+    //        db.Transakcje.Remove(transakcja);
+    //        db.SaveChanges();
+
+    //        return Ok(transakcja);
+    //    }
+
+    //    protected override void Dispose(bool disposing)
+    //    {
+    //        if (disposing)
+    //        {
+    //            db.Dispose();
+    //        }
+    //        base.Dispose(disposing);
+    //    }
+
+    //    private bool TransakcjaExists(int id)
+    //    {
+    //        return db.Transakcje.Count(e => e.idTransakcji == id) > 0;
+    //    }
+    }
+}
