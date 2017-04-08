d2c2dea17e981561a302d772447506d6a659fde5
SideswipeN7 <pawel.wanot@gmail.com>
2017-04-08 22:16:17 +0200

    #71 Small console application for TDD added

---
+++ Client/TDD/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using Client;
+
+namespace TDD
+{
+    [TestFixture]
+    class Program
+    {
+        //
+        int mock;
+        [SetUp]
+        public void Setup()
+        {
+            mock = 0;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
