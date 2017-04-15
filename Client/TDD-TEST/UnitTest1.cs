85398293fccce5996dd8a9ed1b8bf4adc448c678
youkero <ryszardjanicki1@gmail.com>
2017-04-15 12:47:39 +0200

    #72 #66
    Update method ChangeCategory and create tests

--- Client/TDD-TEST/UnitTest1.cs
+++ Client/TDD-TEST/UnitTest1.cs
@@ -50,6 +50,29 @@ namespace TDD_TEST
             result.Should().BeFalse();
         }
 
+        [Test]
+        [TestCaseSource(typeof(UnitTest1), nameof(UnitTest1.TestChangeCategoryCases))]
+        public void ChangeCategory(Kategoria kategoria)
+        {
+            bool result = comm.ChangeCategory(kategoria);
 
+            if (kategoria.idKategorii == 1)
+            {
+                result.Should().BeTrue();
+            }
+
+            if (kategoria.idKategorii == 999)
+            {
+                result.Should().BeFalse();
+            }
+        }
+
+        //Test cases
+        public static Kategoria[] TestChangeCategoryCases =
+        {
+            new Kategoria { idKategorii = 1, Nazwa = "Komputery"},
+            new Kategoria { idKategorii = 999, Nazwa = "Test"}
+
+        };
     }
 }
