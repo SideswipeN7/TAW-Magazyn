498c58851cd9340444906b5c2713070286f545fc
michal81818 <michal81818@gmail.com>
2017-05-03 16:14:28 +0200

    #79

--- Client/Client/MainWindow.xaml.cs
+++ Client/Client/MainWindow.xaml.cs
@@ -3,6 +3,11 @@ using System;
 using System.IO;
 using System.Reflection;
 using PluginExecutor;
+using System.Collections;
+using Client.Model;
+using System.Collections.Generic;
+using System.Threading;
+using System.Data;
 
 namespace Client
 {
@@ -14,6 +19,47 @@ namespace Client
         public MainWindow()
         {
             InitializeComponent();
+            btndodaj.Visibility = Visibility.Collapsed;
+            txt_nazwa.Visibility = Visibility.Collapsed;
+            btnmodyfikuj.Visibility = Visibility.Collapsed;
+            btnusun.Visibility = Visibility.Collapsed;
+            cb_kat_id.Visibility = Visibility.Collapsed;
+        }
+
+        private void rb_kat_dodaj_Checked(object sender, RoutedEventArgs e)
+        {
+            btndodaj.Visibility = Visibility.Visible;
+            txt_nazwa.Visibility = Visibility.Visible;
+            cb_kat_id.Visibility = Visibility.Collapsed;
+            btnusun.Visibility = Visibility.Collapsed;
+            btnmodyfikuj.Visibility = Visibility.Collapsed;
+        }
+
+        private void rb_kat_wszystko_Checked(object sender, RoutedEventArgs e)
+        {
+            btndodaj.Visibility = Visibility.Collapsed;
+            txt_nazwa.Visibility = Visibility.Collapsed;
+            btnmodyfikuj.Visibility = Visibility.Collapsed;
+            btnusun.Visibility = Visibility.Collapsed;
+            cb_kat_id.Visibility = Visibility.Collapsed;
+        }
+
+        private void rb_kat_modyfikuj_Checked(object sender, RoutedEventArgs e)
+        {
+            cb_kat_id.Visibility = Visibility.Visible;
+            btnmodyfikuj.Visibility = Visibility.Visible;
+            btnusun.Visibility = Visibility.Collapsed;
+            btndodaj.Visibility = Visibility.Collapsed;
+            txt_nazwa.Visibility = Visibility.Visible;
+        }
+
+        private void rb_kat_usun_Checked(object sender, RoutedEventArgs e)
+        {
+            btnusun.Visibility = Visibility.Visible;
+            txt_nazwa.Visibility = Visibility.Visible;
+            cb_kat_id.Visibility = Visibility.Visible;
+            btndodaj.Visibility = Visibility.Collapsed;
+            btnmodyfikuj.Visibility = Visibility.Collapsed;
         }
     }
 }
