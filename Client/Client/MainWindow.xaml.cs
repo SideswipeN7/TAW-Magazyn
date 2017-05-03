930f04f321463de34fc8fd5a974d98073253e7b7
michal81818 <michal81818@gmail.com>
2017-05-03 17:44:35 +0200

    #80

--- Client/Client/MainWindow.xaml.cs
+++ Client/Client/MainWindow.xaml.cs
@@ -14,6 +14,92 @@ namespace Client
         public MainWindow()
         {
             InitializeComponent();
+            lblkategoria.Visibility = Visibility.Collapsed;
+            lblnazwa.Visibility = Visibility.Collapsed;
+            lblcena.Visibility = Visibility.Collapsed;
+            lblilosc.Visibility = Visibility.Collapsed;
+
+            btndodaj.Visibility = Visibility.Collapsed;
+            btnmodyfikuj.Visibility = Visibility.Collapsed;
+            btnusun.Visibility = Visibility.Collapsed;
+
+            txt_nazwa.Visibility = Visibility.Collapsed;
+            txt_cena.Visibility = Visibility.Collapsed;
+            txt_ilosc.Visibility = Visibility.Collapsed;
+
+            cb_nazwakat.Visibility = Visibility.Collapsed;
+        }
+
+        private void rb_kat_wszystko_Checked(object sender, RoutedEventArgs e)
+        {
+            lblkategoria.Visibility = Visibility.Collapsed;
+            lblnazwa.Visibility = Visibility.Collapsed;
+            lblcena.Visibility = Visibility.Collapsed;
+            lblilosc.Visibility = Visibility.Collapsed;
+
+            btndodaj.Visibility = Visibility.Collapsed;
+            btnmodyfikuj.Visibility = Visibility.Collapsed;
+            btnusun.Visibility = Visibility.Collapsed;
+
+            txt_nazwa.Visibility = Visibility.Collapsed;
+            txt_cena.Visibility = Visibility.Collapsed;
+            txt_ilosc.Visibility = Visibility.Collapsed;
+
+            cb_nazwakat.Visibility = Visibility.Collapsed;
+        }
+
+        private void rb_kat_dodaj_Checked(object sender, RoutedEventArgs e)
+        {
+            lblkategoria.Visibility = Visibility.Visible;
+            lblnazwa.Visibility = Visibility.Visible;
+            lblcena.Visibility = Visibility.Visible;
+            lblilosc.Visibility = Visibility.Visible;
+
+            btndodaj.Visibility = Visibility.Visible;
+            btnmodyfikuj.Visibility = Visibility.Collapsed;
+            btnusun.Visibility = Visibility.Collapsed;
+
+            txt_nazwa.Visibility = Visibility.Visible;
+            txt_cena.Visibility = Visibility.Visible;
+            txt_ilosc.Visibility = Visibility.Visible;
+
+            cb_nazwakat.Visibility = Visibility.Visible;
+        }
+
+        private void rb_kat_modyfikuj_Checked(object sender, RoutedEventArgs e)
+        {
+            lblkategoria.Visibility = Visibility.Visible;
+            lblnazwa.Visibility = Visibility.Visible;
+            lblcena.Visibility = Visibility.Visible;
+            lblilosc.Visibility = Visibility.Visible;
+
+            btndodaj.Visibility = Visibility.Collapsed;
+            btnmodyfikuj.Visibility = Visibility.Visible;
+            btnusun.Visibility = Visibility.Collapsed;
+
+            txt_nazwa.Visibility = Visibility.Visible;
+            txt_cena.Visibility = Visibility.Visible;
+            txt_ilosc.Visibility = Visibility.Visible;
+
+            cb_nazwakat.Visibility = Visibility.Visible;
+        }
+
+        private void rb_kat_usun_Checked(object sender, RoutedEventArgs e)
+        {
+            lblkategoria.Visibility = Visibility.Collapsed;
+            lblnazwa.Visibility = Visibility.Visible;
+            lblcena.Visibility = Visibility.Collapsed;
+            lblilosc.Visibility = Visibility.Collapsed;
+
+            btndodaj.Visibility = Visibility.Collapsed;
+            btnmodyfikuj.Visibility = Visibility.Collapsed;
+            btnusun.Visibility = Visibility.Visible;
+
+            txt_nazwa.Visibility = Visibility.Visible;
+            txt_cena.Visibility = Visibility.Collapsed;
+            txt_ilosc.Visibility = Visibility.Collapsed;
+
+            cb_nazwakat.Visibility = Visibility.Collapsed;
         }
     }
 }
