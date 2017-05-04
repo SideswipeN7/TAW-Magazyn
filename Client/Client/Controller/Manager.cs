05355dd8f62a8188a38e7a42241519801b249150
SideswipeN7 <pawel.wanot@gmail.com>
2017-05-04 18:00:01 +0200

    #83 few methods done

---
+++ Client/Client/Controller/Manager.cs
@@ -0,0 +1,173 @@
+﻿using Client.Communication;
+using Client.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Controller
+{
+    public sealed class Manager :IManager
+    {
+        private static Manager _instance;
+        private MainWindow _window;
+        private ICommunication _comm;
+
+        private Manager()
+        {
+            _comm = Communicator.GetInstance();
+            _comm.SetUrlAddress("http://o1018869-001-site1.htempurl.com");
+        }
+        public static Manager GetInstance(MainWindow window)
+        {
+            if (_instance == null)
+            {
+                _instance = new Manager();
+            }
+            _instance._window = window;
+            return _instance;
+        }
+
+
+        public void ChangeCategoryData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeClientData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeItemData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetCategoryData()
+        {
+            Task<IEnumerable<Kategoria>>.Factory.StartNew(() =>
+            {
+                return _comm.GetCategories();                
+            }).ContinueWith(x =>
+            {
+                Task.Factory.StartNew(() =>
+                {                  
+                    ShowCategoryData(x.Result);
+                });
+            });
+        }
+        private void ShowCategoryData(IEnumerable<Kategoria> categories)
+        {
+            _window.Dispatcher.BeginInvoke(new Action(() =>
+            {               
+                foreach (Kategoria r in categories)
+                    _window.DgCategoryLista.Items.Add(r);
+
+            }));
+        }
+
+        public void GetClientData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetClientTransactionData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetItemData()
+        {
+            Task<IEnumerable<Artykul>>.Factory.StartNew(() =>
+            {
+                return _comm.GetItems();
+            }).ContinueWith(x =>
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    ShowItemsData(x.Result);
+                });
+            });
+        }
+
+        private void ShowItemsData(IEnumerable<Artykul> categories)
+        {
+            _window.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                foreach (Artykul r in categories)
+                    _window.DgItemLista.Items.Add(r);
+
+            }));
+        }
+
+        public void GetMagazineState()
+        {
+            Task<IEnumerable<Artykul>>.Factory.StartNew(() =>
+            {
+                return _comm.GetItems();
+            }).ContinueWith(x =>
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    ShowMagazineStateData(x.Result);
+                });
+            });
+        }
+        private void ShowMagazineStateData(IEnumerable<Artykul> categories)
+        {
+            _window.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                foreach (Artykul r in categories)
+                    _window.DgStateLista.Items.Add(r);
+
+            }));
+        }
+
+        public void SetCategoryData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetClientData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetClientTransactionData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetItemData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCategoryData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateClientData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateClientTransactionData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItemData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateMagazineState()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Client/Client/MainWindow.xaml.cs
+++ Client/Client/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using Client.Model;
 using System.Collections.Generic;
 using System.Threading;
 using System.Data;
+using Client.Controller;
 
 namespace Client
 {
@@ -16,12 +17,56 @@ namespace Client
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Manager _controller;
         public MainWindow()
         {
             InitializeComponent();
-           
+            _controller = Manager.GetInstance(this);
+            //Show Data
+            _controller.GetCategoryData();
+            _controller.GetMagazineState();
+            _controller.GetItemData();
         }
 
-        
+        //Magazine State
+        private void RbStateSzukaj_Checked(object sender, RoutedEventArgs e)
+        {
+            ChbStateCena.Visibility = Visibility.Visible;
+            ChbStateIlosc.Visibility = Visibility.Visible;
+            ChbStateKategoria.Visibility = Visibility.Visible;
+            ChbStateNazwa.Visibility = Visibility.Visible;
+
+            LblStateCena.Visibility = Visibility.Visible;
+            LblStateIlosc.Visibility = Visibility.Visible;
+            LblStateKategoria.Visibility = Visibility.Visible;
+            LblStateNazwa.Visibility = Visibility.Visible;
+
+            TxbStateCena.Visibility = Visibility.Visible;
+            TxbStateIlosc.Visibility = Visibility.Visible;
+            TxbStateNazwa.Visibility = Visibility.Visible;
+            CmbStateKategoria.Visibility = Visibility.Visible;
+
+            BtnSateSzukaj.Visibility = Visibility.Visible;
+        }
+
+        private void RbStateDodaj_Checked(object sender, RoutedEventArgs e)
+        {
+            ChbStateCena.Visibility = Visibility.Hidden;
+            ChbStateIlosc.Visibility = Visibility.Hidden;
+            ChbStateKategoria.Visibility = Visibility.Hidden;
+            ChbStateNazwa.Visibility = Visibility.Hidden;
+
+            LblStateCena.Visibility = Visibility.Hidden;
+            LblStateIlosc.Visibility = Visibility.Hidden;
+            LblStateKategoria.Visibility = Visibility.Hidden;
+            LblStateNazwa.Visibility = Visibility.Hidden;
+
+            TxbStateCena.Visibility = Visibility.Hidden;
+            TxbStateIlosc.Visibility = Visibility.Hidden;
+            TxbStateNazwa.Visibility = Visibility.Hidden;
+            CmbStateKategoria.Visibility = Visibility.Hidden;
+
+            BtnSateSzukaj.Visibility = Visibility.Hidden;
+        }
     }
 }
