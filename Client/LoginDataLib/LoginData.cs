3fcc691bd069453597f32d5cab4c6a82a70c6792
SideswipeN7 <pawel.wanot@gmail.com>
2017-05-19 08:57:51 +0200

    #90 done

---
+++ Client/LoginDataLib/LoginData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginDataLib
+{
+    public class LoginData
+    {
+      bool Sudo { get; set; }
+      int Id { get; set; }
+    }
+}
