b97412471353943bf6b8a4bd38623e3cc29af2d5
Michał Stanecki <stanustanley@gmail.com>
2017-05-22 20:13:12 +0200

    #104 Window Creators added

---
+++ Client/Client/Windows/Creators.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Client.Windows
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Creators.xaml
+    /// </summary>
+    public partial class Creators : Window
+    {
+        public Creators()
+        {
+            InitializeComponent();
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            this.WindowStyle = WindowStyle.ToolWindow;
+        }
+
+        private void bt_creators_ok_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
