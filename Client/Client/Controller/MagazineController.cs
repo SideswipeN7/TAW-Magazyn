735ae22f682b4cbe70b3106840c793f0485c8476
SideswipeN7 <pawel.wanot@gmail.com>
2017-05-30 21:07:21 +0200

    #111 start

--- Client/Client/Controller/MagazineController.cs
+++ Client/Client/Controller/MagazineController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Client.Windows;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,24 +7,34 @@ using System.Threading.Tasks;
 
 namespace Client.Controller
 {
-    class CategoryController : IWork
+    class MagazineController : IWork
     {
+        private Admin _window { get; set; }
+        private static MagazineController _instance;
+
+        private MagazineController() { }
+
+        public static MagazineController GetInstance(Admin window)
+        {
+            if (_instance == null)
+            {
+                _instance = new MagazineController();
+            }
+            _instance._window = window;
+            return _instance;
+        }
+
         public void AddData()
         {
             throw new NotImplementedException();
         }
 
-        public void ChangeData()
+        public void ChangeDate()
         {
             throw new NotImplementedException();
         }
 
         public void DeleteData()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void SearchData()
         {
             throw new NotImplementedException();
         }
